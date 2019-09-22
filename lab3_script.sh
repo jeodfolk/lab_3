59358006d5bdbb28ca7a9fddfcbc70e883a32791
jeodfolk <woolfolkjoseph1@yahoo.com>
2019-09-21 21:58:48 -0600

    add email_results and lab3_script

--- lab3_script.sh
+++ lab3_script.sh
@@ -2,15 +2,19 @@
 # Authors : Joseph Woolfolk
 # Date: 9/19/2019
 
-#Problem 1 Code: grep cmd with arguements to seach for phone numbers starting with 303 within the regex_practice file
+#Problem 1 Code: grep cmd with arguements to seach for phone numbers starting with 303 and emails
+# within the regex_practice file
 #echo cmds to demonstrate user inputs
 echo "Enter a file name: "
 read filename
 echo "expression: "
 read exp
- 
-grep -c ^303-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9] regex_practice.txt >> email_results.txt
-grep -o ^303-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9] regex_practice.txt >> email_results.txt
+
+echo "303 numbers" 
+grep -c ^303-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9] regex_practice.txt
+grep -o ^303-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9] regex_practice.txt
+
+grep -o .*@[a-zA-Z]*[.][a-zA-Z]*$  regex_practice.txt >> email_results.txt
 
 
 #Make sure to document how you are solving each problem!
